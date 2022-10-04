d293878a3dfdc2f8a6e16041246565a326000e9c
Leland Clemmons <lclemmons@justworks.com>
2022-10-04 08:13:35 -0400

    Create pixel-to-rem converter for certain type values
    Figma only supports pixel and percentage values for line heights
    and font sizes. So we're creating our own support for that here.
    REM values are defined as being a multiplier of the base font
    size.
    
    Since we keep the CSS default of 16px for that base font size
    in Clockwork, we can assume it here too. This *will* cause problems
    if downstream platforms don't use 16px on `<body>`.

---
+++ build.js
@@ -0,0 +1,28 @@
+const StyleDictionary = require("style-dictionary").extend("config.json");
+
+StyleDictionary.registerTransform({
+  name: "typography/px-to-rem",
+  type: "value",
+  matcher(prop) {
+    const transformable =
+      prop.attributes.type === "type" &&
+      (prop.attributes.item === "line-height" ||
+        prop.attributes.item === "font-size");
+    // Figma's pixel values are unitless
+    return transformable && !isNaN(prop.original.value);
+  },
+  transformer(prop) {
+    const BASE_FONT_PIXEL_SIZE = 16;
+    const remValue = prop.original.value.toFixed(3) / BASE_FONT_PIXEL_SIZE;
+    return `${remValue.toString()}rem`;
+  },
+});
+
+StyleDictionary.registerTransformGroup({
+  name: "custom/css",
+  transforms: StyleDictionary.transformGroup["css"].concat([
+    "typography/px-to-rem",
+  ]),
+});
+
+StyleDictionary.buildAllPlatforms();
